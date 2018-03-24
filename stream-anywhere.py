c1f041cf1a4930b24c54d3567f21b78fbf604341
Nekmo <contacto@nekmo.com>
2018-03-24 18:32:14 +0100

    All project.

---
+++ stream-anywhere.py
@@ -0,0 +1,58 @@
+from flask import Flask, url_for, send_from_directory, request
+from flask_restful import Api, Resource, reqparse, abort
+
+from db import get_or_create
+from models import create_session, Video
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return send_from_directory('static/stream-anywhere/src', 'index.html')
+
+
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('url')
+
+
+class VideoApi(Resource):
+    def get_object(self, video_id, session=None):
+        session = session or create_session()
+        video, exists = get_or_create(session, Video, id=int(video_id))
+        if not exists:
+            abort(404)
+        return video
+
+    def get(self, video_id):
+        return self.get_object(video_id).json()
+
+    def patch(self, video_id):
+        session = create_session()
+        video = self.get_object(video_id, session)
+        for key, value in request.json.items():
+            setattr(video, key, value)
+        session.commit()
+
+
+class VideoListApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+    def post(self):
+        args = parser.parse_args()
+        session = create_session()
+        video, exists = get_or_create(session, Video, url=args.url)
+        if not exists:
+            session.commit()
+        return dict(is_new=not exists, **video.json())
+
+
+api.add_resource(VideoListApi, '/api/videos')
+api.add_resource(VideoApi, '/api/videos/<video_id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
